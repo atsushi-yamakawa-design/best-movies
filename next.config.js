0bf56412f685c08350d63f147bb814718406f373
atsushi-yamakawa-design <96972564+atsushi-yamakawa-design@users.noreply.github.com>
2024-01-06 06:05:34 +0900

    CORSを許可

--- next.config.js
+++ next.config.js
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // 対象APIのパスパターン
+        // 今回は src/app/api/ 配下にAPIを作っているので下記のようにする
+        source: "/api.themoviedb.org/:path*",
+        headers: [
+          {
+            // CORSを許可するオリジン
+            key: "Access-Control-Allow-Origin",
+            // すべてのオリジンを許可するなら * (アスタリスク)
+            // ただセキュリティ的にはよろしくないので注意
+            value: "https://best-movies-sand.vercel.app/"
+          },
+          {
+            // 許可するメソッド
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,POST"
+          },
+          {
+            // 許可するリクエストヘッダ
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type"
+          }
+        ]
+      }
+    ];
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

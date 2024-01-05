7f2003956a369b59f96419a51c068223cbaca9a8
atsushi-yamakawa-design <96972564+atsushi-yamakawa-design@users.noreply.github.com>
2024-01-06 03:10:29 +0900

    シェアテキストをprops化

--- src/components/ShareImage.tsx
+++ src/components/ShareImage.tsx
@@ -5,12 +5,14 @@ interface ImagePageProps {
   backgroundUrl: string;
   movieImageUrls: string[];
   movieTitles: string[];
+  shareText: string;
 }
 
 const ShareImage = ({
   backgroundUrl,
   movieImageUrls,
-  movieTitles
+  movieTitles,
+  shareText
 }: ImagePageProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -21,9 +23,9 @@ const ShareImage = ({
       const imagesToLoad = [backgroundUrl, ...movieImageUrls];
       const imagePositions = [
         { x: 0, y: 0 }, // 背景画像
-        { x: 220, y: 400 },
-        { x: 220, y: 710 },
-        { x: 220, y: 1020 }
+        { x: 220, y: 380 },
+        { x: 220, y: 690 },
+        { x: 220, y: 1000 }
       ];
 
       if (ctx) {
@@ -65,18 +67,18 @@ const ShareImage = ({
                     title,
                     textX,
                     position.y,
-                    600,
-                    60,
+                    640,
+                    70,
                     true // 実際に描画せずにテキストの高さを計算
                   );
 
                   // テキストを画像の中央に配置
                   const textY =
                     position.y + imageHeight / 2 - textHeight / 2 + 40;
-                  wrapText(ctx, title, textX, textY, 700, 60); // 実際に描画
+                  wrapText(ctx, title, textX, textY, 640, 70); // 実際に描画
                 } else {
                   // 4番目以降のテキスト
-                  let startY = 880 + 170 * 3;
+                  let startY = 840 + 170 * 3;
 
                   movieTitles.slice(3).forEach((title, index) => {
                     const dynamicTitle = `${index + 4}. ${title}`;
@@ -93,7 +95,7 @@ const ShareImage = ({
                     );
 
                     // 次のテキストのY座標を更新
-                    startY += textHeight + 30;
+                    startY += textHeight + 20;
                   });
                 }
               });
@@ -113,8 +115,8 @@ const ShareImage = ({
           navigator
             .share({
               files: [file],
-              title: "Canvas Image",
-              text: "Check out this image I created!"
+              title: "My Best Movies 2023 ",
+              text: shareText
             })
             .catch((error) => {
               console.error("Error sharing the image", error);
@@ -152,49 +154,72 @@ function wrapText(
   let newHeight = 0;
   const words = text.split(" ");
   let line = "";
+  let lineCount = 0; // 行数をカウントする変数
 
-  for (let n = 0; n < words.length; n++) {
-    const word = words[n];
-    const testLine = line + word + " ";
-    const metrics = context.measureText(testLine);
-    const testWidth = metrics.width;
-
-    if (testWidth > maxWidth && n > 0) {
-      // 単語単位で改行
+  for (const word of words) {
+    if (lineCount === 2) {
+      // 2行を超えたら省略記号を追加して処理を終了
+      line += "...";
       if (!dryRun) {
         context.fillText(line, x, y);
       }
-      line = word + " ";
-      y += lineHeight;
       newHeight += lineHeight;
-    } else if (metrics.width > maxWidth) {
-      // 文字単位で改行
-      for (const char of word) {
-        const testLineWithChar = line + char;
-        const metricsWithChar = context.measureText(testLineWithChar);
+      break;
+    }
 
-        if (metricsWithChar.width > maxWidth) {
+    // 日本語か英語かによって処理を変える
+    if (isJapanese(word)) {
+      // 日本語の場合は文字単位で改行
+      for (const char of word) {
+        const testLine = line + char;
+        if (context.measureText(testLine).width > maxWidth) {
           if (!dryRun) {
             context.fillText(line, x, y);
           }
           line = char;
           y += lineHeight;
           newHeight += lineHeight;
+          lineCount++;
+          if (lineCount === 2) {
+            // 2行目に達したら次のループで省略記号を追加
+            break;
+          }
         } else {
-          line = testLineWithChar;
+          line = testLine;
         }
       }
       line += " ";
     } else {
-      line = testLine;
+      // 英語の単語の場合は単語単位で改行
+      const testLine = line + word + " ";
+      if (context.measureText(testLine).width > maxWidth) {
+        if (!dryRun) {
+          context.fillText(line, x, y);
+        }
+        line = word + " ";
+        y += lineHeight;
+        newHeight += lineHeight;
+        lineCount++;
+        if (lineCount === 2) {
+          // 2行目に達したら次のループで省略記号を追加
+          break;
+        }
+      } else {
+        line = testLine;
+      }
     }
   }
 
   // 最後の行を描画
-  if (!dryRun && line) {
+  if (!dryRun && line.trim()) {
     context.fillText(line.trim(), x, y);
     newHeight += lineHeight;
   }
 
   return newHeight;
+}
+
+// 日本語かどうかを判断する簡単な関数
+function isJapanese(text: string) {
+  return /[\u3000-\u30FF\uFF00-\uFFEF\u4E00-\u9FAF\u3400-\u4DBF]/.test(text);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -157,6 +157,7 @@ export default function MoviePage() {
               backgroundUrl={backgroundUrl}
               movieImageUrls={movieImageUrls}
               movieTitles={selectedMovieTitles}
+              shareText="テストです〜"
             />
             <button onClick={handleEditList} className={style.editList}>
               ← リストを編集する

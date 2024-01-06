6a52851a37c041716d8e06305fc56379e8e6c34d
atsushi-yamakawa-design <96972564+atsushi-yamakawa-design@users.noreply.github.com>
2024-01-06 16:55:48 +0900

    プロキシサーバーの設定変更

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-// server.js
-const express = require("express");
-const next = require("next");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+// // server.js
+// const express = require("express");
+// const next = require("next");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+// const dev = process.env.NODE_ENV !== "production";
+// const app = next({ dev });
+// const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express();
+// app.prepare().then(() => {
+//   const server = express();
 
-  // プロキシ設定
-  server.use(
-    "/api",
-    createProxyMiddleware({
-      target: "https://api.themoviedb.org", // プロキシ先のURL
-      changeOrigin: true,
-      pathRewrite: {
-        "^/api": "" // /apiを取り除く
-      }
-    })
-  );
+//   // プロキシ設定
+//   server.use(
+//     "/api",
+//     createProxyMiddleware({
+//       target: "https://api.themoviedb.org", // プロキシ先のURL
+//       changeOrigin: true,
+//       pathRewrite: {
+//         "^/api": "" // /apiを取り除く
+//       }
+//     })
+//   );
 
-  server.all("*", (req, res) => {
-    return handle(req, res);
-  });
+//   server.all("*", (req, res) => {
+//     return handle(req, res);
+//   });
 
-  const PORT = process.env.PORT || 3000;
-  server.listen(PORT, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${PORT}`);
-  });
-});
+//   const PORT = process.env.PORT || 3000;
+//   server.listen(PORT, (err) => {
+//     if (err) throw err;
+//     console.log(`> Ready on http://localhost:${PORT}`);
+//   });
+// });

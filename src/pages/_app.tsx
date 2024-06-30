0f4b07bc43155e4a4e6ba509b3ebd07cb06da51c
atsushi-yamakawa-design <96972564+atsushi-yamakawa-design@users.noreply.github.com>
2024-06-30 17:57:48 +0900

    Gタグ追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,32 @@ import type { AppProps } from "next/app";
 import "../styles/reset.css";
 import "../styles/global.scss";
 import "../styles/variables.scss";
+import Head from "next/head";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-XFXB6NPH3Z"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-XFXB6NPH3Z');
+            `
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
